89f45d9f172d539f05f81d6b952632fae981a58c
lcqychl <1403774897@qq.com>
2019-08-01 21:42:38 +0800

    20190801

---
+++ src/leetCode/Test0016.java
@@ -0,0 +1,34 @@
+package leetCode;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个包括n个整数的数组nums和 一个目标值target。找出nums中的三个整数，
+ * 使得它们的和与target最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+ * 例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
+ * 与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
+ */
+
+public class Test0016 {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closestNum = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; i++) {
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                int threeSum = nums[l] + nums[r] + nums[i];
+                if (Math.abs(threeSum - target) < Math.abs(closestNum - target)) {
+                    closestNum = threeSum;
+                }
+                if (threeSum > target) {
+                    r--;
+                } else if (threeSum < target) {
+                    l++;
+                } else {
+                    return target;
+                }
+            }
+        }
+        return closestNum;
+    }
+}
