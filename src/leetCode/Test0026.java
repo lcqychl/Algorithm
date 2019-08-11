36f41688896eabc7b00a94801a781cefb17f2f9a
lcqychl <1403774897@qq.com>
2019-08-11 22:23:30 +0800

    20190811

---
+++ src/leetCode/Test0026.java
@@ -0,0 +1,30 @@
+package leetCode;
+
+/**
+ * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ * 示例 1:
+ * 给定数组 nums = [1,1,2],
+ * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+ * 你不需要考虑数组中超出新长度后面的元素。
+ */
+
+public class Test0026 {
+    public int removeDuplicates(int[] nums) {
+        // 使用双指针
+        if (nums == null || nums.length == 1) {
+            return nums.length;
+        }
+        int i = 0, j = 1;
+        while (j < nums.length) {
+            if (nums[i] == nums[j]) {
+                j++;
+            } else {
+                i++;
+                nums[i] = nums[j];
+                j++;
+            }
+        }
+        return i + 1;
+    }
+}
