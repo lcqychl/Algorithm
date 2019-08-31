35c08a7e4bcaa97d2c1a84650f93ff9792aaa43a
lcqychl <1403774897@qq.com>
2019-08-31 23:08:47 +0800

    20190831

---
+++ src/leetCode/test1_400/test0041_0060/Test0046.java
@@ -0,0 +1,45 @@
+package leetCode.test1_400.test0041_0060;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个没有重复数字的序列，返回其所有可能的全排列。
+ * 示例:
+ * 输入: [1,2,3]
+ * 输出:
+ * [
+ * [1,2,3],
+ * [1,3,2],
+ * [2,1,3],
+ * [2,3,1],
+ * [3,1,2],
+ * [3,2,1]
+ * ]
+ */
+
+public class Test0046 {
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        int[] visited = new int[nums.length];
+        backtrack(res, nums, new ArrayList<Integer>(), visited);
+        return res;
+
+    }
+
+    private void backtrack(List<List<Integer>> res, int[] nums, ArrayList<Integer> tmp, int[] visited) {
+        if (tmp.size() == nums.length) {
+            res.add(new ArrayList<>(tmp));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i] == 1) continue;
+            visited[i] = 1;
+            tmp.add(nums[i]);
+            backtrack(res, nums, tmp, visited);
+            visited[i] = 0;
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+}
