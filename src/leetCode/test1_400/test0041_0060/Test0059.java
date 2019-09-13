15aecea4355f44a463b1724e9177dbe7f5d1e9d1
lcqychl <1403774897@qq.com>
2019-09-13 23:27:05 +0800

    20190912

---
+++ src/leetCode/test1_400/test0041_0060/Test0059.java
@@ -0,0 +1,32 @@
+package leetCode.test1_400.test0041_0060;
+
+/**
+ * 给定一个正整数 n，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵。
+ * 示例:
+ * 输入: 3
+ * 输出:
+ * [
+ * [ 1, 2, 3 ],
+ * [ 8, 9, 4 ],
+ * [ 7, 6, 5 ]
+ * ]
+ */
+
+public class Test0059 {
+    public int[][] generateMatrix(int n) {
+        int l = 0, r = n - 1, t = 0, b = n - 1;
+        int[][] mat = new int[n][n];
+        int num = 1, tar = n * n;
+        while (num <= tar) {
+            for (int i = l; i <= r; i++) mat[t][i] = num++; // left to right.
+            t++;
+            for (int i = t; i <= b; i++) mat[i][r] = num++; // top to bottom.
+            r--;
+            for (int i = r; i >= l; i--) mat[b][i] = num++; // right to left.
+            b--;
+            for (int i = b; i >= t; i--) mat[i][l] = num++; // bottom to top.
+            l++;
+        }
+        return mat;
+    }
+}
