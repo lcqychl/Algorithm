8ff453afd93e0756640e4d238d6b266402878f10
lcqychl <1403774897@qq.com>
2019-10-24 21:37:31 +0800

    20191024

---
+++ src/leetCode/test1_400/Test0081_0100/Test0100.java
@@ -0,0 +1,49 @@
+package leetCode.test1_400.Test0081_0100;
+
+/**
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ * 示例 1:
+ * 输入:       1         1
+ * / \       / \
+ * 2   3     2   3
+ * <p>
+ * [1,2,3],   [1,2,3]
+ * 输出: true
+ * 示例 2:
+ * 输入:      1          1
+ * /           \
+ * 2             2
+ * <p>
+ * [1,2],     [1,null,2]
+ * 输出: false
+ * 示例 3:
+ * 输入:       1         1
+ * / \       / \
+ * 2   1     1   2
+ * <p>
+ * [1,2,1],   [1,1,2]
+ * 输出: false
+ */
+
+public class Test0100 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // p and q are both null
+        if (p == null && q == null) return true;
+        // one of p and q is null
+        if (q == null || p == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.right, q.right) &&
+                isSameTree(p.left, q.left);
+    }
+}
