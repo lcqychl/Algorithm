edde7212e32fdc9e775dc88031e7425971dab5b2
lcqychl <1403774897@qq.com>
2019-11-22 20:47:38 +0800

    20191122

---
+++ src/leetCode/test1_400/test0001_0020/Test0006.java
@@ -0,0 +1,43 @@
+package leetCode.test1_400.test0001_0020;
+
+/*
+*将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+L   C   I   R
+E T O E S I I G
+E   D   H   N
+之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+请你实现这个将字符串进行指定行数变换的函数：
+string convert(string s, int numRows);
+示例 1:
+输入: s = "LEETCODEISHIRING", numRows = 3
+输出: "LCIRETOESIIGEDHN"
+示例 2:
+输入: s = "LEETCODEISHIRING", numRows = 4
+输出: "LDREOEIIECIHNTSG"
+解释:
+L     D     R
+E   O E   I I
+E C   I H   N
+T     S     G
+* */
+
+public class Test0006 {
+    public String convert(String s, int numRows) {
+
+        if (numRows == 1) return s;
+
+        StringBuilder ret = new StringBuilder();
+        int n = s.length();
+        int cycleLen = 2 * numRows - 2;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < n; j += cycleLen) {
+                ret.append(s.charAt(j + i));
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
+                    ret.append(s.charAt(j + cycleLen - i));
+            }
+        }
+        return ret.toString();
+    }
+}
