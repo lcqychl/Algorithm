8acdaae9611d3f810d58a60b685217c9179538a9
lcqychl <1403774897@qq.com>
2019-11-24 16:28:18 +0800

    20191124

---
+++ src/leetCode/test1_400/test0001_0020/Test0004.java
@@ -0,0 +1,37 @@
+package leetCode.test1_400.test0001_0020;
+
+/**
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+ * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ * 你可以假设 nums1 和 nums2 不会同时为空。
+ * 示例 1:
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ * 则中位数是 2.0
+ * 示例 2:
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ * 则中位数是 (2 + 3)/2 = 2.5
+ */
+
+public class Test0004 {
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        int len = m + n;
+        int left = -1, right = -1;
+        int aStart = 0, bStart = 0;
+        for (int i = 0; i <= len / 2; i++) {
+            left = right;
+            if (aStart < m && (bStart >= n || A[aStart] < B[bStart])) {
+                right = A[aStart++];
+            } else {
+                right = B[bStart++];
+            }
+        }
+        if ((len & 1) == 0)
+            return (left + right) / 2.0;
+        else
+            return right;
+    }
+}
