22528cbeeeb934db94fffd57f43db4d9c5ba1e14
lcqychl <1403774897@qq.com>
2019-12-01 19:17:04 +0800

    20191201

---
+++ src/leetCode/test1_400/Test0121_0140/Test0125.java
@@ -0,0 +1,27 @@
+package leetCode.test1_400.Test0121_0140;
+
+/*
+* 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+说明：本题中，我们将空字符串定义为有效的回文串。
+示例 1:
+输入: "A man, a plan, a canal: Panama"
+输出: true
+示例 2:
+输入: "race a car"
+输出: false
+* */
+
+public class Test0125 {
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while(i < j){
+            while(i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+            while(i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
