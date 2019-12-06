5adb0327842e356b9c5d0708f92c69c5650a5f44
nimingxiong <1403774897@qq.com>
2019-12-06 19:33:45 +0800

    20191206

--- src/leetCode/test1_400/Test0121_0140/Test0132.java
+++ src/leetCode/test1_400/Test0121_0140/Test0132.java
@@ -16,32 +16,56 @@ package leetCode.test1_400.Test0121_0140;
 
 public class Test0132 {
     public int minCut(String s) {
-        boolean[][] dp = new boolean[s.length()][s.length()];
-        int length = s.length();
+        int[] dp = new int[s.length()];
+        int n = s.length();
+        //假设没有任何的回文串，初始化 dp
+        for (int i = 0; i < n; i++) {
+            dp[i] = i;
+        }
 
-        for (int len = 1; len <= length; len++) {
-            for (int i = 0; i <= s.length() - len; i++) {
-                int j = i + len - 1;
-                dp[i][j] = s.charAt(i) == s.charAt(j) && (len < 3 || dp[i + 1][j - 1]);
+        // 考虑每个中心
+        for (int i = 0; i < s.length(); i++) {
+            // j 表示某一个方向扩展的个数
+            int j = 0;
+            // 考虑奇数的情况
+            while (true) {
+                if (i - j < 0 || i + j > n - 1) {
+                    break;
+                }
+                if (s.charAt(i - j) == s.charAt(i + j)) {
+                    if (i - j == 0) {
+                        dp[i + j] = 0;
+                    } else {
+                        dp[i + j] = Math.min(dp[i + j], dp[i - j - 1] + 1);
+                    }
+
+                } else {
+                    break;
+                }
+                j++;
             }
-        }
-        minCutHelper(s, 0, dp, 0);
-        return min;
 
-    }
+            // j 表示某一个方向扩展的个数
+            j = 1;
+            // 考虑偶数的情况
+            while (true) {
+                if (i - j + 1 < 0 || i + j > n - 1) {
+                    break;
+                }
+                if (s.charAt(i - j + 1) == s.charAt(i + j)) {
+                    if (i - j + 1 == 0) {
+                        dp[i + j] = 0;
+                    } else {
+                        dp[i + j] = Math.min(dp[i + j], dp[i - j + 1 - 1] + 1);
+                    }
 
-    int min = Integer.MAX_VALUE;
-    //num 记录已经切割的次数
-    private void minCutHelper(String s, int start, boolean[][] dp, int num) {
-        if (dp[start][s.length() - 1]) {
-            min = Math.min(min, num);
-            return;
-        }
-        //尝试当前字符串所有的切割位置
-        for (int i = start; i < s.length() - 1; i++) {
-            if (dp[start][i]) {
-                minCutHelper(s, i + 1, dp, num + 1);
+                } else {
+                    break;
+                }
+                j++;
             }
+
         }
+        return dp[n - 1];
     }
 }
