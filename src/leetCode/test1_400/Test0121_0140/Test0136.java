27950128dbf4e74a810b2cc567197766a67b9859
nimingxiong <1403774897@qq.com>
2019-12-12 19:52:09 +0800

    20191212

---
+++ src/leetCode/test1_400/Test0121_0140/Test0136.java
@@ -0,0 +1,25 @@
+package leetCode.test1_400.Test0121_0140;
+
+/**
+ * @author nimingxiong
+ * @date 2019/12/12 19:50
+ *
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ * 说明：
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ * 示例 1:
+ * 输入: [2,2,1]
+ * 输出: 1
+ * 示例 2:
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ */
+
+public class Test0136 {
+    public int singleNumber(int[] nums) {
+        for(int i=1; i<nums.length; i++) {
+            nums[0] = nums[0] ^ nums[i];
+        }
+        return nums[0];
+    }
+}
