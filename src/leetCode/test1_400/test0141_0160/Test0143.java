0fe8a5a380d00e17f9d0ac944449c9020ae32d9f
nimingxiong <1403774897@qq.com>
2019-12-20 15:07:11 +0800

    20191220

--- src/leetCode/test1_400/test0141_0160/Test0143.java
+++ src/leetCode/test1_400/test0141_0160/Test0143.java
@@ -1,77 +1,73 @@
 package leetCode.test1_400.test0141_0160;
 
-import java.util.HashMap;
-
 /**
  * @author nimingxiong
- * @date 2019/12/19 19:10
- *
- * 给定一个二维平面，平面上有 n 个点，求最多有多少个点在同一条直线上。
- * 示例 1:
- * 输入: [[1,1],[2,2],[3,3]]
- * 输出: 3
- * 解释:
- * ^
- * |
- * |        o
- * |     o
- * |  o  
- * +------------->
- * 0  1  2  3  4
- * 示例 2:
- * 输入: [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]
- * 输出: 4
- * 解释:
- * ^
- * |
- * |  o
- * |     o        o
- * |        o
- * |  o        o
- * +------------------->
- * 0  1  2  3  4  5  6
+ * @date 2019/12/20 19:37
+ * 
+ * 给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+ * 将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * 示例 1:
+ * 给定链表 1->2->3->4, 重新排列为 1->4->2->3.
+ * 示例 2:
+ * 给定链表 1->2->3->4->5, 重新排列为 1->5->2->4->3.
  */
 public class Test0143 {
-    public int maxPoints(int[][] points) {
-        if (points.length < 3) {
-            return points.length;
-        }
-        int res = 0;
-        //遍历每个点
-        for (int i = 0; i < points.length; i++) {
-            int duplicate = 0;
-            int max = 0;//保存经过当前点的直线中，最多的点
-            HashMap<String, Integer> map = new HashMap<>();
-            for (int j = i + 1; j < points.length; j++) {
-                //求出分子分母
-                int x = points[j][0] - points[i][0];
-                int y = points[j][1] - points[i][1];
-                if (x == 0 && y == 0) {
-                    duplicate++;
-                    continue;
+    public class ListNode {
+        int val;
+        ListNode next;
 
-                }
-                //进行约分
-                int gcd = gcd(x, y);
-                x = x / gcd;
-                y = y / gcd;
-                String key = x + "@" + y;
-                map.put(key, map.getOrDefault(key, 0) + 1);
-                max = Math.max(max, map.get(key));
-            }
-            //1 代表当前考虑的点，duplicate 代表和当前的点重复的点
-            res = Math.max(res, max + duplicate + 1);
+        ListNode(int x) {
+            val = x;
         }
-        return res;
     }
 
-    private int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = a % b;
-            a = b;
-            b = temp;
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return;
         }
-        return a;
+        //找中点，链表分成两个
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode newHead = slow.next;
+        slow.next = null;
+
+        //第二个链表倒置
+        newHead = reverseList(newHead);
+
+        //链表节点依次连接
+        while (newHead != null) {
+            ListNode temp = newHead.next;
+            newHead.next = head.next;
+            head.next = newHead;
+            head = newHead.next;
+            newHead = temp;
+        }
+
+    }
+
+    private ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode tail = head;
+        head = head.next;
+
+        tail.next = null;
+
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = tail;
+            tail = head;
+            head = temp;
+        }
+
+        return tail;
     }
 
 }
---
+++ src/leetCode/test1_400/test0141_0160/Test0149.java
@@ -0,0 +1,77 @@
+package leetCode.test1_400.test0141_0160;
+
+import java.util.HashMap;
+
+/**
+ * @author nimingxiong
+ * @date 2019/12/19 19:10
+ *
+ * 给定一个二维平面，平面上有 n 个点，求最多有多少个点在同一条直线上。
+ * 示例 1:
+ * 输入: [[1,1],[2,2],[3,3]]
+ * 输出: 3
+ * 解释:
+ * ^
+ * |
+ * |        o
+ * |     o
+ * |  o  
+ * +------------->
+ * 0  1  2  3  4
+ * 示例 2:
+ * 输入: [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]
+ * 输出: 4
+ * 解释:
+ * ^
+ * |
+ * |  o
+ * |     o        o
+ * |        o
+ * |  o        o
+ * +------------------->
+ * 0  1  2  3  4  5  6
+ */
+public class Test0149 {
+    public int maxPoints(int[][] points) {
+        if (points.length < 3) {
+            return points.length;
+        }
+        int res = 0;
+        //遍历每个点
+        for (int i = 0; i < points.length; i++) {
+            int duplicate = 0;
+            int max = 0;//保存经过当前点的直线中，最多的点
+            HashMap<String, Integer> map = new HashMap<>();
+            for (int j = i + 1; j < points.length; j++) {
+                //求出分子分母
+                int x = points[j][0] - points[i][0];
+                int y = points[j][1] - points[i][1];
+                if (x == 0 && y == 0) {
+                    duplicate++;
+                    continue;
+
+                }
+                //进行约分
+                int gcd = gcd(x, y);
+                x = x / gcd;
+                y = y / gcd;
+                String key = x + "@" + y;
+                map.put(key, map.getOrDefault(key, 0) + 1);
+                max = Math.max(max, map.get(key));
+            }
+            //1 代表当前考虑的点，duplicate 代表和当前的点重复的点
+            res = Math.max(res, max + duplicate + 1);
+        }
+        return res;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
+}
