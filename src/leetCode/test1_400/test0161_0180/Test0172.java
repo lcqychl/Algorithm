988eaf0dd9ba3a41064045e6cfb9b352a3017418
nimingxiong <1403774897@qq.com>
2020-01-16 11:27:39 +0800

    20200116

---
+++ src/leetCode/test1_400/test0161_0180/Test0172.java
@@ -0,0 +1,26 @@
+package leetCode.test1_400.test0161_0180;
+
+/**
+ * @author nimingxiong
+ * Created at 2020/1/16 11:20
+ *
+ * 给定一个整数 n，返回 n! 结果尾数中零的数量。
+ * 示例 1:
+ * 输入: 3
+ * 输出: 0
+ * 解释: 3! = 6, 尾数中没有零。
+ * 示例 2:
+ * 输入: 5
+ * 输出: 1
+ * 解释: 5! = 120, 尾数中有 1 个零.
+ */
+public class Test0172 {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while (n > 0) {
+            count += n / 5;
+            n = n / 5;
+        }
+        return count;
+    }
+}
