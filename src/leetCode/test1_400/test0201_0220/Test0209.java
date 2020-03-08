0b42a56e6a5c1e494d334aa6316673257540eada
lcqychl <1403774897@qq.com>
2020-03-08 19:37:37 +0800

    20200308

---
+++ src/leetCode/test1_400/test0201_0220/Test0209.java
@@ -0,0 +1,38 @@
+package leetCode.test1_400.test0201_0220;
+
+/**
+ * @author nimingxiong
+ * Created at 2020/3/8 19:34
+ *
+ * 给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的连续子数组。
+ * 如果不存在符合条件的连续子数组，返回 0。
+ * 示例: 
+ * 输入: s = 7, nums = [2,3,1,2,4,3]
+ * 输出: 2
+ * 解释: 子数组 [4,3] 是该条件下的长度最小的连续子数组。
+ * 进阶:
+ * 如果你已经完成了O(n) 时间复杂度的解法, 请尝试 O(n log n) 时间复杂度的解法。
+ */
+
+public class Test0209 {
+    public int minSubArrayLen(int s, int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        while (right < n) {
+            sum += nums[right];
+            right++;
+            while (sum >= s) {
+                min = Math.min(min, right - left);
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return min == Integer.MAX_VALUE ? 0 : min;
+    }
+}
