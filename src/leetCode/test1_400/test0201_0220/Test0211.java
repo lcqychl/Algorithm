b843f7a41301ab37cda48048bf85c12b3cf07fb2
lcqychl <1403774897@qq.com>
2020-03-10 21:23:58 +0800

    20200310

---
+++ src/leetCode/test1_400/test0201_0220/Test0211.java
@@ -0,0 +1,92 @@
+package leetCode.test1_400.test0201_0220;
+
+/**
+ * @author nimingxiong
+ * Created at 2020/3/10 21:19
+ *
+ * 设计一个支持以下两种操作的数据结构：
+ * void addWord(word)
+ * bool search(word)
+ * search(word) 可以搜索文字或正则表达式字符串，字符串只包含字母 . 或 a-z 。 . 可以表示任何一个字母。
+ * 示例:
+ * addWord("bad")
+ * addWord("dad")
+ * addWord("mad")
+ * search("pad") -> false
+ * search("bad") -> true
+ * search(".ad") -> true
+ * search("b..") -> true
+ * 说明:
+ * 你可以假设所有单词都是由小写字母 a-z 组成的。
+ */
+
+public class Test0211 {
+    class TrieNode {
+        TrieNode[] children;
+        boolean flag;
+
+        public TrieNode() {
+            children = new TrieNode[26];
+            flag = false;
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+
+    }
+    TrieNode root;
+
+    /** Initialize your data structure here. */
+    public Test0211() {
+        root = new TrieNode();
+    }
+
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+        char[] array = word.toCharArray();
+        TrieNode cur = root;
+        for (int i = 0; i < array.length; i++) {
+            // 当前孩子是否存在
+            if (cur.children[array[i] - 'a'] == null) {
+                cur.children[array[i] - 'a'] = new TrieNode();
+            }
+            cur = cur.children[array[i] - 'a'];
+        }
+        // 当前节点代表结束
+        cur.flag = true;
+    }
+
+    /**
+     * Returns if the word is in the data structure. A word could contain the
+     * dot character '.' to represent any one letter.
+     */
+    public boolean search(String word) {
+        return searchHelp(word, root);
+    }
+
+    private boolean searchHelp(String word, TrieNode root) {
+        char[] array = word.toCharArray();
+        TrieNode cur = root;
+        for (int i = 0; i < array.length; i++) {
+            // 对于 . , 递归的判断所有不为空的孩子
+            if(array[i] == '.'){
+                for(int j = 0;j < 26; j++){
+                    if(cur.children[j] != null){
+                        if(searchHelp(word.substring(i + 1),cur.children[j])){
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+            // 不含有当前节点
+            if (cur.children[array[i] - 'a'] == null) {
+                return false;
+            }
+            cur = cur.children[array[i] - 'a'];
+        }
+        // 当前节点是否为是某个单词的结束
+        return cur.flag;
+    }
+
+}
