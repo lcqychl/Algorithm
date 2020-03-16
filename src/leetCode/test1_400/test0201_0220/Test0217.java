3d9853ec97d4e90a88e22d5354804446c270cfdf
lcqychl <1403774897@qq.com>
2020-03-16 21:04:41 +0800

    20200316

---
+++ src/leetCode/test1_400/test0201_0220/Test0217.java
@@ -0,0 +1,32 @@
+package leetCode.test1_400.test0201_0220;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author nimingxiong
+ * Created at 2020/3/16 20:59
+ *
+ * 给定一个整数数组，判断是否存在重复元素。
+ * 如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
+ * 示例 1:
+ * 输入: [1,2,3,1]
+ * 输出: true
+ * 示例 2:
+ * 输入: [1,2,3,4]
+ * 输出: false
+ * 示例 3:
+ * 输入: [1,1,1,3,3,4,3,2,4,2]
+ * 输出: true
+ */
+
+public class Test0217 {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>(nums.length);
+        for (int x : nums) {
+            if (set.contains(x)) return true;
+            set.add(x);
+        }
+        return false;
+    }
+}
