445370f577ffc68a767ee3f827920e1a5f78cdd6
lcqychl <1403774897@qq.com>
2020-03-25 21:36:07 +0800

    20200325

---
+++ src/leetCode/test1_400/test0221_0240/Test0226.java
@@ -0,0 +1,37 @@
+package leetCode.test1_400.test0221_0240;
+
+import leetCode.TreeNode;
+
+/**
+ * @author nimingxiong
+ * Created at 2020/3/25 21:30
+ *
+ * 翻转一棵二叉树。
+ * 示例：
+ * 输入：
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 输出：
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ */
+
+public class Test0226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode right = invertTree(root.right);
+        TreeNode left = invertTree(root.left);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
