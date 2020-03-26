453ba0ff08789e39f01878a3627aa2930059dbe9
lcqychl <1403774897@qq.com>
2020-03-26 22:25:29 +0800

    20200326

---
+++ src/leetCode/test1_400/test0221_0240/Test0227.java
@@ -0,0 +1,65 @@
+package leetCode.test1_400.test0221_0240;
+
+import java.util.Stack;
+
+/**
+ * @author nimingxiong
+ * Created at 2020/3/26 22:10
+ *
+ * 实现一个基本的计算器来计算一个简单的字符串表达式的值。
+ *
+ * 字符串表达式仅包含非负整数，+， - ，*，/ 四种运算符和空格  。 整数除法仅保留整数部分。
+ *
+ * 示例 1:
+ *
+ * 输入: "3+2*2"
+ * 输出: 7
+ * 示例 2:
+ *
+ * 输入: " 3/2 "
+ * 输出: 1
+ * 示例 3:
+ *
+ * 输入: " 3+5 / 2 "
+ * 输出: 5
+ * 说明：
+ *
+ * 你可以假设所给定的表达式都是有效的。
+ * 请不要使用内置的库函数 eval
+ */
+
+public class Test0227 {
+    public int calculate(String s) {
+        Stack<Integer> numStack = new Stack<>();
+
+        char lastOp = '+';
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == ' ') continue;
+
+            if (Character.isDigit(arr[i])) {
+                int tempNum = arr[i] - '0';
+                while (++i < arr.length && Character.isDigit(arr[i])) {
+                    tempNum = tempNum * 10 + (arr[i] - '0');
+                }
+                i--;
+
+                if (lastOp == '+') numStack.push(tempNum);
+                else if (lastOp == '-') numStack.push(-tempNum);
+                else numStack.push(res(lastOp, numStack.pop(), tempNum));
+            } else lastOp = arr[i];
+        }
+
+        int ans = 0;
+        for (int num : numStack) ans += num;
+        return ans;
+    }
+
+    private int res(char op, int a, int b) {
+        if (op == '*') return a * b;
+        else if (op == '/') return a / b;
+        else if (op == '+') return a + b; //其实加减运算可以忽略
+        else return a - b;
+    }
+
+}
