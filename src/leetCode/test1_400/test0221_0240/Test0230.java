d4a62e52eef750a1136259430674703336b65a6d
nimingxiong <1403774897@qq.com>
2020-03-31 19:55:06 +0800

    20200331

---
+++ src/leetCode/test1_400/test0221_0240/Test0230.java
@@ -0,0 +1,49 @@
+package leetCode.test1_400.test0221_0240;
+
+import leetCode.TreeNode;
+
+import java.util.LinkedList;
+
+/**
+ * @author nimingxiong
+ * Created at 2020/3/31 19:49
+ *
+ * 给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
+ * 说明：
+ * 你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
+ * 示例 1:
+ * 输入: root = [3,1,4,null,2], k = 1
+ *    3
+ *   / \
+ *  1   4
+ *   \
+ *    2
+ * 输出: 1
+ * 示例 2:
+ * 输入: root = [5,3,6,2,4,null,null,1], k = 3
+ *        5
+ *       / \
+ *      3   6
+ *     / \
+ *    2   4
+ *   /
+ *  1
+ * 输出: 3
+ * 进阶：
+ * 如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化 kthSmallest 函数？
+ */
+public class Test0230 {
+    public int kthSmallest(TreeNode root, int k) {
+        LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
+
+        while (true) {
+            while (root != null) {
+                stack.add(root);
+                root = root.left;
+            }
+            root = stack.removeLast();
+            if (--k == 0) return root.val;
+            root = root.right;
+        }
+    }
+}
