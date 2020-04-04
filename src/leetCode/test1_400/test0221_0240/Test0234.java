fa0c79d3c730af5e71fd3379ba9c84afab49b0df
lcqychl <1403774897@qq.com>
2020-04-04 21:25:56 +0800

    20200404

---
+++ src/leetCode/test1_400/test0221_0240/Test0234.java
@@ -0,0 +1,59 @@
+package leetCode.test1_400.test0221_0240;
+
+import leetCode.ListNode;
+
+/**
+ * @author nimingxiong
+ * Created at 2020/4/4 21:21
+ *
+ * 请判断一个链表是否为回文链表。
+ * 示例 1:
+ * 输入: 1->2
+ * 输出: false
+ * 示例 2:
+ * 输入: 1->2->2->1
+ * 输出: true
+ */
+
+public class Test0234 {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) return true;
+
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result = true;
+        while (result && p2 != null) {
+            if (p1.val != p2.val) result = false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        firstHalfEnd.next = reverseList(secondHalfStart);
+        return result;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    private ListNode endOfFirstHalf(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
