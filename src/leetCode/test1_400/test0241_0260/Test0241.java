bca6ac6c24e76abfaf8528e3c647aa5d90a125eb
nimingxiong <1403774897@qq.com>
2020-04-11 19:54:33 +0800

    20200411

---
+++ src/leetCode/test1_400/test0241_0260/Test0241.java
@@ -0,0 +1,93 @@
+package leetCode.test1_400.test0241_0260;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author nimingxiong
+ * Created at 2020/4/11 19:49
+ *
+ * 给定一个含有数字和运算符的字符串，为表达式添加括号，改变其运算优先级以求出不同的结果。
+ * 你需要给出所有可能的组合的结果。有效的运算符号包含 +, - 以及 * 。
+ * 示例 1:
+ * 输入: "2-1-1"
+ * 输出: [0, 2]
+ * 解释:
+ * ((2-1)-1) = 0
+ * (2-(1-1)) = 2
+ * 示例 2:
+ * 输入: "2*3-4*5"
+ * 输出: [-34, -14, -10, -10, 10]
+ * 解释:
+ * (2*(3-(4*5))) = -34
+ * ((2*3)-(4*5)) = -14
+ * ((2*(3-4))*5) = -10
+ * (2*((3-4)*5)) = -10
+ * (((2*3)-4)*5) = 10
+ */
+public class Test0241 {
+    public List<Integer> diffWaysToCompute(String input) {
+        List<Integer> numList = new ArrayList<>();
+        List<Character> opList = new ArrayList<>();
+        char[] array = input.toCharArray();
+        int num = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (isOperation(array[i])) {
+                numList.add(num);
+                num = 0;
+                opList.add(array[i]);
+                continue;
+            }
+            num = num * 10 + array[i] - '0';
+        }
+        numList.add(num);
+        int N = numList.size();
+
+
+        ArrayList<Integer>[][] dp = (ArrayList<Integer>[][]) new ArrayList[N][N];
+        for (int i = 0; i < N; i++) {
+            ArrayList<Integer> result = new ArrayList<>();
+            result.add(numList.get(i));
+            dp[i][i] = result;
+        }
+
+        for (int n = 2; n <= N; n++) {
+            for (int i = 0; i < N; i++) {
+                int j = i + n - 1;
+                if (j >= N) {
+                    break;
+                }
+                ArrayList<Integer> result = new ArrayList<>();
+                for (int s = i; s < j; s++) {
+                    ArrayList<Integer> result1 = dp[i][s];
+                    ArrayList<Integer> result2 = dp[s + 1][j];
+                    for (int x = 0; x < result1.size(); x++) {
+                        for (int y = 0; y < result2.size(); y++) {
+                            char op = opList.get(s);
+                            result.add(caculate(result1.get(x), op, result2.get(y)));
+                        }
+                    }
+                }
+                dp[i][j] = result;
+
+            }
+        }
+        return dp[0][N - 1];
+    }
+
+    private int caculate(int num1, char c, int num2) {
+        switch (c) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+        }
+        return -1;
+    }
+
+    private boolean isOperation(char c) {
+        return c == '+' || c == '-' || c == '*';
+    }
+}
