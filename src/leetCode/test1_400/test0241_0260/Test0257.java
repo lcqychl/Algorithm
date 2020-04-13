40a13c2fa34c2a019fe2c0e65903ef4d31cf1c74
nimingxiong <1403774897@qq.com>
2020-04-13 21:56:46 +0800

    20200413

---
+++ src/leetCode/test1_400/test0241_0260/Test0257.java
@@ -0,0 +1,52 @@
+package leetCode.test1_400.test0241_0260;
+
+import leetCode.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author nimingxiong
+ * Created at 2020/4/13 21:54
+ *
+ * 给定一个二叉树，返回所有从根节点到叶子节点的路径。
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 示例:
+ * 输入:
+ *    1
+ *  /   \
+ * 2     3
+ *  \
+ *   5
+ * 输出: ["1->2->5", "1->3"]
+ * 解释: 所有根节点到叶子节点的路径为: 1->2->5, 1->3
+ */
+public class Test0257 {
+    public List<String> binaryTreePaths(TreeNode root) {
+        LinkedList<String> paths = new LinkedList();
+        if (root == null)
+            return paths;
+
+        LinkedList<TreeNode> node_stack = new LinkedList();
+        LinkedList<String> path_stack = new LinkedList();
+        node_stack.add(root);
+        path_stack.add(Integer.toString(root.val));
+        TreeNode node;
+        String path;
+        while (!node_stack.isEmpty()) {
+            node = node_stack.pollLast();
+            path = path_stack.pollLast();
+            if ((node.left == null) && (node.right == null))
+                paths.add(path);
+            if (node.left != null) {
+                node_stack.add(node.left);
+                path_stack.add(path + "->" + Integer.toString(node.left.val));
+            }
+            if (node.right != null) {
+                node_stack.add(node.right);
+                path_stack.add(path + "->" + Integer.toString(node.right.val));
+            }
+        }
+        return paths;
+    }
+}
