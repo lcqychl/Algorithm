313bb11bff2f1d43af4670ab7bab63d2f2dc79b9
nimingxiong <1403774897@qq.com>
2020-05-04 13:54:58 +0800

    20200504

---
+++ src/leetCode/test1_400/test0301_0320/Test0302.java
@@ -0,0 +1,30 @@
+package leetCode.test1_400.test0301_0320;
+
+/**
+ * @author nimingxiong
+ * Created at 2020/5/4 13:53
+ *
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
+ * 示例：
+ * 给定 nums = [-2, 0, 3, -5, 2, -1]，求和函数为 sumRange()
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ * 说明:
+ * 你可以假设数组不可变。
+ * 会多次调用 sumRange 方法。
+ */
+public class Test0302 {
+    private int[] sum;
+
+    public Test0302(int[] nums) {
+        sum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sum[j + 1] - sum[i];
+    }
+}
