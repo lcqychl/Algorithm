ef47e0d2af68edd7d11baaaf3494666b90524457
nimingxiong <1403774897@qq.com>
2020-06-15 19:54:09 +0800

    20200615

---
+++ src/leetCode/test1_400/test0341_0360/Test0350.java
@@ -0,0 +1,41 @@
+package leetCode.test1_400.test0341_0360;
+
+import java.util.Arrays;
+
+/**
+ * @author nimingxiong
+ * Created at 2020/6/15 19:52
+ *
+ *给定两个数组，编写一个函数来计算它们的交集。
+ * 示例 1:
+ * 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出: [2,2]
+ * 示例 2:
+ * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出: [4,9]
+ * 说明：
+ * 输出结果中每个元素出现的次数，应与元素在两个数组中出现的次数一致。
+ * 我们可以不考虑输出结果的顺序。
+ * 进阶:
+ * 如果给定的数组已经排好序呢？你将如何优化你的算法？
+ * 如果 nums1 的大小比 nums2 小很多，哪种方法更优？
+ * 如果 nums2 的元素存储在磁盘上，磁盘内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
+ */
+public class Test0350 {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0, k = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                ++i;
+            } else if (nums1[i] > nums2[j]) {
+                ++j;
+            } else {
+                nums1[k++] = nums1[i++];
+                ++j;
+            }
+        }
+        return Arrays.copyOfRange(nums1, 0, k);
+    }
+}
