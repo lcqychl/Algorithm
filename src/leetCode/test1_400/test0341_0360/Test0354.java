100517fcc41283eee377f6ab768612cba8070e16
nimingxiong <1403774897@qq.com>
2020-06-17 19:56:17 +0800

    20200617

---
+++ src/leetCode/test1_400/test0341_0360/Test0354.java
@@ -0,0 +1,52 @@
+package leetCode.test1_400.test0341_0360;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @author nimingxiong
+ * Created at 2020/6/17 19:52
+ *
+ * 给定一些标记了宽度和高度的信封，宽度和高度以整数对形式 (w, h) 出现。
+ * 当另一个信封的宽度和高度都比这个信封大的时候，这个信封就可以放进另一个信封里，如同俄罗斯套娃一样。
+ * 请计算最多能有多少个信封能组成一组“俄罗斯套娃”信封（即可以把一个信封放到另一个信封里面）。
+ * 说明:
+ * 不允许旋转信封。
+ * 示例:
+ * 输入: envelopes = [[5,4],[6,4],[6,7],[2,3]]
+ * 输出: 3
+ * 解释: 最多信封的个数为 3, 组合为: [2,3] => [5,4] => [6,7]。
+ */
+public class Test0354 {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
+        for (int num : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, num);
+            if (i < 0) {
+                i = -(i + 1);
+            }
+            dp[i] = num;
+            if (i == len) {
+                len++;
+            }
+        }
+        return len;
+    }
+
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            public int compare(int[] arr1, int[] arr2) {
+                if (arr1[0] == arr2[0]) {
+                    return arr2[1] - arr1[1];
+                } else {
+                    return arr1[0] - arr2[0];
+                }
+            }
+        });
+
+        int[] secondDim = new int[envelopes.length];
+        for (int i = 0; i < envelopes.length; ++i) secondDim[i] = envelopes[i][1];
+        return lengthOfLIS(secondDim);
+    }
+}
