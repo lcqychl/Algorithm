3eff9b8bfe9da8b1fa4ca3480ec0274df45bca46
nimingxiong <1403774897@qq.com>
2020-07-01 20:27:50 +0800

    20200701

---
+++ src/leetCode/test1_400/test0361_0380/Test0374.java
@@ -0,0 +1,50 @@
+package leetCode.test1_400.test0361_0380;
+
+/**
+ * @author nimingxiong
+ * Created at 2020/7/1 20:19
+ *
+ * 我们正在玩一个猜数字游戏。 游戏规则如下：
+ * 我从 1 到 n 选择一个数字。 你需要猜我选择了哪个数字。
+ * 每次你猜错了，我会告诉你这个数字是大了还是小了。
+ * 你调用一个预先定义好的接口 guess(int num)，它会返回 3 个可能的结果（-1，1 或 0）：
+ * -1 : 我的数字比较小
+ *  1 : 我的数字比较大
+ *  0 : 恭喜！你猜对了！
+ * 示例 :
+ * 输入: n = 10, pick = 6
+ * 输出: 6
+ */
+public class Test0374 {
+    public int guessNumber(int n) {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid1 = low + (high - low) / 3;
+            int mid2 = high - (high - low) / 3;
+            int res1 = guess(mid1);
+            int res2 = guess(mid2);
+            if (res1 == 0)
+                return mid1;
+            if (res2 == 0)
+                return mid2;
+            else if (res1 < 0)
+                high = mid1 - 1;
+            else if (res2 > 0)
+                low = mid2 + 1;
+            else {
+                low = mid1 + 1;
+                high = mid2 - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int defaultNum = 50;
+
+    public static int guess(int guess) {
+        return Integer.compare(guess, defaultNum);
+
+    }
+
+}
